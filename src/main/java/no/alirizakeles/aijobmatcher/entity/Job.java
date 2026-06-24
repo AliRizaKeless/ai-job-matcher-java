a4eccd88ae1d866725d7896664a72fe703a1eaa0
Ali Riza Keles <alirizakeles66@gmail.com>
2026-06-24 15:23:47 +0200

    Add Job entity and database mapping

---
+++ src/main/java/no/alirizakeles/aijobmatcher/entity/Job.java
@@ -0,0 +1,24 @@
+package no.alirizakeles.aijobmatcher.entity;
+
+import jakarta.persistence.Entity;
+import lombok.Data;
+import jakarta.persistence.Id;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Table;
+
+@Data
+@Entity
+@Table(name = "jobs")
+public class Job {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String title;
+    private String companyName;
+    private String location;
+    private String description;
+    private String url;
+}
