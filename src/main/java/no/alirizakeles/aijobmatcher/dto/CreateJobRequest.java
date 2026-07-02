e44694abbd9bc3b36726bc76025e153748128d4c
Ali Riza Keles <alirizakeles66@gmail.com>
2026-07-02 09:15:37 +0200

    Add validation for create job request

--- src/main/java/no/alirizakeles/aijobmatcher/dto/CreateJobRequest.java
+++ src/main/java/no/alirizakeles/aijobmatcher/dto/CreateJobRequest.java
@@ -1,14 +1,25 @@
 package no.alirizakeles.aijobmatcher.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
 import lombok.Data;
 
 @Data
 public class CreateJobRequest {
 
+    @NotBlank
     private String title;
+
+    @NotBlank
     private String companyName;
+
+    @NotBlank
     private String location;
+
+    @Size(max = 5000)
     private String description;
+
     private String url;
+
     private String employmentType;
 }
